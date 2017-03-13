3eb9ba103212439bffd97e9187bba723ca333f35
HarnonA <harnonanizio@gmail.com>
2017-03-13 13:45:35 -0300

    Update Regiao.java

--- RSSF/src/Regiao.java
+++ RSSF/src/Regiao.java
@@ -6,8 +6,26 @@ public class Regiao {
 	Point inf;
 	
 	
-	public Regiao(){
-		
+	public Regiao(Point sup, Point inf){
+		this.sup = sup;
+		this.inf = inf;
+	}
+	
+	public boolean estaDentro(Point no){
+		//no dentro do limite de x
+		if(no.getX()>= sup.getX() && no.getX() <= inf.getX()){
+			//no dentro do limite de y
+			if(no.getY() >= inf.getY() && no.getY() <= sup.getY())
+				return true;
+		}
+		return false;		
+	}
+	
+	public int nNosDentro(){
+		//for lista de nos
+		//if no dentro de x e y
+		//cont++
+		return 0;
 	}
 	
 	public int getRegiao(){
