d0a4423eee6417a975f87bef1637becb1653b80c
Felipe Emidio <lipe.emidio@gmail.com>
2017-03-14 03:40:20 -0300

    Update Regiao.java

--- RSSF/src/Regiao.java
+++ RSSF/src/Regiao.java
@@ -1,43 +1,20 @@
+package RSSF;
 import java.awt.Point;
 
 public class Regiao {
-	int idReg;
-	Point sup;
-	Point inf;
-	
+	private Point sup;
+	private Point inf;
 	
 	public Regiao(Point sup, Point inf){
-		this.sup = sup;
-		this.inf = inf;
-	}
-	
-	public boolean estaDentro(Point no){
-		//no dentro do limite de x
-		if(no.getX()>= sup.getX() && no.getX() <= inf.getX()){
-			//no dentro do limite de y
-			if(no.getY() >= inf.getY() && no.getY() <= sup.getY())
-				return true;
-		}
-		return false;		
-	}
-	
-	public int nNosDentro(Node no){
-		int cont = 0;
-		for (int i = 0; i < no.vizinhos.size(); i++) {
-			//if no dentro da regiao
-			if(estaDentro(no.vizinhos.get(i).posicao))
-				cont++;	
-		}
-		return cont;
-	}
-	
-	public int getRegiao(){
-		return idReg;
-	}
-	
-	public void subreg(Regiao r){
-		
-		
+		this.sup = sup; // Superior esquerdo.
+		this.inf = inf; // Inferior direiro.
 	}
 
+	public Point getSup(){
+		return this.sup;
+	}
+	
+	public Point getInf(){
+		return this.inf;
+	}
 }
